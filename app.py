2c235e72ac5134fa732d6b11d69e430c9f87ea82
Mario Gutiérrez <mariogutierrezcto@gmail.com>
2025-07-05 12:58:07 +0200

    New design

--- app.py
+++ app.py
@@ -4,23 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return redirect(url_for('home'))
-
-@app.route('/home')
-def home():
     return render_template('home.html')
-
-@app.route('/portfolio')
-def portfolio():
-    return render_template('portfolio.html')
-
-@app.route('/cv')
-def cv():
-    return render_template('cv.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
